31d2e181ec17590072ec1df61c050d9e44d0afe9
MyBruddhah <minh804.nguyen@gmail.com>
2026-02-19 14:45:03 +0100

    Ajout signature de fonction sans implémation dans StudentService + Création de getter et setter dans Student

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,25 @@
 public class Student {
     private Integer id;
     private String name;
+
+    public Student(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
---
+++ src/StudentService.java
@@ -0,0 +1,14 @@
+public class StudentService {
+
+    public void printList() {
+
+    }
+
+    public void addStudent(int id, String name) {
+
+    }
+
+    public void removeStudent(int id) {
+
+    }
+}
