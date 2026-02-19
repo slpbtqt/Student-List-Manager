4f40370d395320079366decb4cf51a9627cbc76b
Samy Ben Arbia <samy.bnrb@gmail.com>
2026-02-19 15:32:59 +0100

    Création du main CLI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
 public class Main{
-    public static void main(String[] arg){
-
+    boolean stop;
+    public static void main(String[] args){
+        boolean stop = false;
+        while (stop == false){
+            Scanner myObj = new Scanner(System.in);  // Scanner
+            System.out.println("Student List Manager\n" + "Type 'help' to show commands.");
+            String command = myObj.nextLine();  // Read user input
+            String listeee[] = command.split(" ", command.length());
+            if (listeee[0] == "help"){
+                System.out.println("Available commands:\n" + "  list\n" + "  add <id> <name>\n" + "  remove <id>\n" + "  export <format> <file_path>  (formats: csv, json)\n" + "  help\n" + "  quit");
+            }
+            else if (listeee[0] ==  "quit") {
+                stop = true;
+            }
+            else if (listeee[0] == "add"){
+                StudentService.addStudent(listeee[1], listeee[2]);
+            }
+            else if (listeee[0] == "remove"){
+                StudentService.removeStudent(listeee[1]);
+            }
+            else if (listeee[0] == "list"){
+                StudentService.printList();
+            }
+            else{
+                System.out.println("this command does not exist")
+            }
+        }
     }
 }
